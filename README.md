5908a6da647336b2aaab2e4e5a0f1eeb3d1fa1a8
jefersonprimer <jefersonpriimer@gmail.com>
2025-02-20 14:10:06 -0300

    feat: connection with mongoDB

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 DTO -> Data Transfer Object, sem regras de negocios, apenas para transportar os dados/modelar o body
 
 mongo db = mongodb://root:root@localhost:27017/nest?authSource=admin&directConnection=true
-docker compose para a image
+docker compose para a image -> docker compose up
--- src/assets/entities/asset.entity.ts
+++ src/assets/entities/asset.entity.ts
@@ -1,1 +1,18 @@
-export class Asset {}
+import { Schema } from '@nestjs/mongoose';
+
+@Schema({ timestamps: true })
+export class Asset {
+    @Prop()
+    _id: string;
+
+    name: string;
+
+    symbol: string;
+
+    image: string;
+
+    price: number;
+
+    createAt!: Date;
+    upadateAt!: Date;
+}
