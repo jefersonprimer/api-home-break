a3beb9ef6e67d5ed87e31f7f2bc66f5cf9559745
jefersonpriimer <jefersonpriimer@gmail.com>
2025-02-20 14:29:46 +0000

    feat: assets crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AssetsModule } from './assets/assets.module';
 
 @Module({
-  imports: [],
   controllers: [AppController],
   providers: [AppService],
+  imports: [AssetsModule],
 })
 export class AppModule {}
