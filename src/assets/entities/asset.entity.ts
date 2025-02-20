13e33d84e23f42031a3f2d21df4d41634dfeacbd
jefersonpriimer <jefersonpriimer@gmail.com>
2025-02-20 17:36:09 +0000

    feat: asset.entity prop

--- src/assets/entities/asset.entity.ts
+++ src/assets/entities/asset.entity.ts
@@ -1,18 +1,28 @@
-import { Schema } from '@nestjs/mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from "mongoose"
+import crypto from "crypto"
+
+export type AssetDocument = HydratedDocument<Asset>;
 
 @Schema({ timestamps: true })
 export class Asset {
-    @Prop()
+    @Prop({ default: () => crypto.randomUUID() })
     _id: string;
 
+    @Prop()
     name: string;
 
+    @Prop()
     symbol: string;
 
+    @Prop()
     image: string;
 
+    @Prop()
     price: number;
 
     createAt!: Date;
     upadateAt!: Date;
 }
+
+export const AssetSchema = SchemaFactory.createForClass(Asset);
